99bb9ca5e2601a9651a7ad028f620759f0880c4e
Henry Rosario <henryrosario056@gmail.com>
2024-03-31 10:56:41 -0400

    Cambios UI

---
+++ TareaDomestica-master/src/data/Tarea.java
@@ -0,0 +1,61 @@
+package data;
+
+public class Tarea {
+    private int tareaId = 0;
+    private int empleadoId = 0;
+    private String descripcion = "";
+    private String fecha = "";
+    private String nombre = "";
+    private String estado = "por hacer";
+
+    public Tarea() {
+    }
+
+    public int getTareaId() {
+        return tareaId;
+    }
+
+    public void setTareaId(int tareaId) {
+        this.tareaId = tareaId;
+    }
+
+    public int getEmpleadoId() {
+        return empleadoId;
+    }
+
+    public void setEmpleadoId(int empleadoId) {
+        this.empleadoId = empleadoId;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
